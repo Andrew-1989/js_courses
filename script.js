86e051d834f70ecb44535088c6cd6ca2ecfe0d2f
Andrew <mido2.holopov@gmail.com>
2019-12-23 22:52:52 +0300

    lesson2_2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let money = prompt("Ваш бюджет на месяц?", ''),
+var money = + prompt("Ваш бюджет на месяц?", ''), // '+' преобразует тип даных в Number
 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-let appData = {
+var appData = {
 	budget: money,
 	expenses: {},
 	optionalExpenses: {},
@@ -10,13 +10,6 @@ let appData = {
 	savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
-
-appData.expenses.a1 = a2;  
-appData.expenses.a3 = a4;
 /*
 Есть переменная let a = 'first' и let b = 'second'; Обе содержат строки. И есть пустой объект let obj = {};
 Этот объект мы можем заполнить данными из переменных a и b. Для этого воспользуемся одним из синтаксисов:
@@ -25,4 +18,34 @@ obj.a = b;
 obj[a] = b;
 */
 
-alert(appData.budget / 30);
+for (var i = 0; i < 2; i++) {
+	var a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+		b = prompt("Во сколько обойдется?", '');
+	if ((typeof (a)) === 'string' && // тип переменной равен string
+		//(typeof (a)) === !null && // тип переменной НЕ равен NULL
+		//(typeof (b)) === !null &&
+		a != '' &&
+		b != '' && // значение переменной не пустое
+		a.length < 50) { // длина введенного значения < 50 символов
+		console.log("done");
+		appData.expenses[a] = b;
+		console.log(appData.expenses[a]);
+	} else {
+
+	}
+
+}
+
+appData.moneyPerDay = appData.budget / 30; // создание нового свойства объекта 
+
+alert("Ежедневный бюджет" + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100 ) {
+	console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+	console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+	console.log("Высокий уровень достатка");
+} else {
+	console.log("Error");
+}
